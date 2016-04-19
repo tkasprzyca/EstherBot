1d9c3b5d510b816fdb5c5154582a46f6fae866ab
ifmTK <tk@tkmbp.local>
2016-04-19 16:08:50 +0200

    0.0.4

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Oh, so your loking for a new oppertunity? Say HELLO to start your jurney')
                 .then(() => 'speak');
         }
     },
